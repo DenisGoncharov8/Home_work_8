3f4663c07061e8b5377193b7f96b1e321cbbc940
Denis Goncharov <goncharov012015@gmail.com>
2021-07-24 21:29:34 +0300

    Initial commit

---
+++ src/MyWindow.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyWindow extends JFrame {
+    public MyWindow() {
+        setVisible(true);
+        setLocation(650, 500);
+        setSize(500, 500);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel(new GridLayout(1, 2));
+
+        JButton btn1 = new JButton("Выход");
+        ActionListener closeListener = new CloseActionListener();
+        btn1.addActionListener(closeListener);
+        panel.add(btn1);
+
+        JButton btn2 = new JButton( "Кнопка");
+        panel.add(btn2);
+
+        add(panel, BorderLayout.SOUTH);
+
+    }
+
+}
